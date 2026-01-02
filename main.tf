8f151c62e226999cb29b55427c17b22436c0afcd
abinsinghk <abinsingh.kumaresan@awaninfotech.com>
2026-01-02 16:37:12 +0530

    fourth commit

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "Appserver" {
   ami           = "resolve:ssm:/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
   instance_type =  tolist(var.ec2_instance_type)[1]
-  region = var.region[2]
+  region = tolist(var.region)[2]
   count = var.config.instance_count
   monitoring = var.config.monitoring
   tags = var.environment_tags
@@ -15,7 +15,7 @@ resource "aws_security_group" "my_first" {
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
-  security_group_id = aws_security_group.myfirst.id
+  security_group_id = aws_security_group.my_first.id
   cidr_ipv4         = var.cidr_range[0]
   from_port         = var.security_group_config[0]
   ip_protocol       = var.security_group_config[1]
