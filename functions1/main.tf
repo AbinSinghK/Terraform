9052461a656ff527717a7486b8dc06343690091e
Abin Singh K <abinsingh2016@gmail.com>
2026-01-06 15:15:11 +0530

    new changes made recently

--- functions1/main.tf
+++ functions1/main.tf
@@ -1,3 +1,9 @@
 locals {
   formatted_project_name = lower(replace(var.project_name," ", "-"))
 }
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket"
+
+  tags = merge(var.default_tags,var.environment_tags)
+}
--- functions1/variables.tf
+++ functions1/variables.tf
@@ -3,3 +3,21 @@ variable "project_name" {
     default = "Prjoject ALPHA Resource"
   
 }
+
+variable "default_tags" {
+
+    default = {
+        company = "awan"
+        managed_by = "terraform"
+    }
+  
+}
+
+variable "environment_tags" {
+
+default = {
+    environment = "production"
+    cost_center = "cc-128"
+}
+
+}
