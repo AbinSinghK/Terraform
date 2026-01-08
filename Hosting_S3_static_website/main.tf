c2471d94dedf0946a07cb0f9aaed35fcaf88c82f
Abin Singh K <abinsingh2016@gmail.com>
2026-01-08 16:26:23 +0530

    static website

--- Hosting_S3_static_website/main.tf
+++ Hosting_S3_static_website/main.tf
@@ -85,7 +85,7 @@ resource "aws_s3_object" "website_files" {
 
 resource "aws_cloudfront_distribution" "s3_distribution" {
   origin {
-    domain_name              = aws_s3_bucket.website.bucket_regional_domain_name
+    domain_name              = aws_s3_bucket.static_website.bucket_regional_domain_name
     origin_access_control_id = aws_cloudfront_origin_access_control.oac.id
     origin_id                = "S3-${aws_s3_bucket.static_website.id}"
   }
