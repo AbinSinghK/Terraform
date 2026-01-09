42a17f828b3aadba96d785d93086d5a78c625f84
Abin Singh K <abinsingh2016@gmail.com>
2026-01-09 15:57:25 +0530

    errors fixed

--- vpc_peering/main.tf
+++ vpc_peering/main.tf
@@ -19,7 +19,7 @@ resource "aws_vpc" "secondary_vpc" {
 resource "aws_subnet" "primary_subnet" {
   provider = aws.primary
   vpc_id     = aws_vpc.primary_vpc.id
-  cidr_block = var.primary_cidr_block
+  cidr_block = var.primary_subnet_cidr
   availability_zone = data.aws_availability_zones.primary.names[0]
   map_public_ip_on_launch = true
 
@@ -29,7 +29,7 @@ resource "aws_subnet" "primary_subnet" {
 resource "aws_subnet" "secondary_subnet" {
   provider = aws.secondary
   vpc_id     = aws_vpc.secondary_vpc.id
-  cidr_block = var.secondary_cidr_block
+  cidr_block = var.secondary_subnet_cidr
   availability_zone = data.aws_availability_zones.secondary.names[0]
   map_public_ip_on_launch = true
 
--- vpc_peering/terraform.tfvars
+++ vpc_peering/terraform.tfvars
@@ -6,6 +6,8 @@ tags = {
 }
 primary_cidr_block = "10.0.0.0/16"
 secondary_cidr_block = "10.1.0.0/16"
+primary_subnet_cidr = "10.0.1.0/24"
+secondary_subnet_cidr = "10.1.1.0/24"
 instance_type = "t2.micro"
 primary_key_name = "vpc-peering-primary"
 secondary_key_name = "vpc-peering-secondary"
