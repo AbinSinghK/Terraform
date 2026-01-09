c81fc9984354d83f6755d58f9da23f64d70f3961
Abin Singh K <abinsingh2016@gmail.com>
2026-01-09 15:10:46 +0530

    code fixed

--- vpc_peering/terraform.tfvars
+++ vpc_peering/terraform.tfvars
@@ -7,3 +7,5 @@ tags = {
 primary_cidr_block = "10.0.0.0/16"
 secondary_cidr_block = "10.1.0.0/16"
 instance_type = "t2.micro"
+primary_key_name = "vpc-peering-primary"
+secondary_key_name = "vpc-peering-secondary"
--- vpc_peering/variables.tf
+++ vpc_peering/variables.tf
@@ -27,3 +27,15 @@ variable "instance_type" {
     type = string
   
 }
+
+variable "primary_key_name" {
+  description = "Name of the SSH key pair for Primary VPC instance (us-east-1)"
+  type        = string
+  
+}
+
+variable "secondary_key_name" {
+  description = "Name of the SSH key pair for Secondary VPC instance (us-west-2)"
+  type        = string
+  
+}
